daa5312f3805fc8777ca8118b4fe0ae27ad1bd0e
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-08 15:14:33 +0900

    add .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'home.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(ProviderScope(child: _App()));
 }
 
