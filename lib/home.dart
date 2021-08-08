575ff7741b98d629a05aa6e72a033736a4562652
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-08 13:42:16 +0900

    add bottom navigation bar

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,22 @@
-
 import 'package:flutter/material.dart';
+import 'package:zinnia/news.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: Center(),
+      body: const NewsPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: 'News'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Category',
+          ),
+        ],
+        onTap: (_) {},
+      ),
     );
   }
 }
