3d0a0f24c21c1f12ef5e09bf2c2dccdf45e95be2
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-14 14:02:42 +0900

    display items

--- lib/news.dart
+++ lib/news.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:zinnia/news_view_model.dart';
 
-class NewsPage extends StatelessWidget {
+class NewsPage extends HookConsumerWidget {
   const NewsPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox.shrink();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final items = ref.watch(
+      newsViewModelProvider.select((value) => value.items),
+    );
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        final item = items[index];
+        return ListTile(
+          title: Text(item.title),
+          subtitle: Text(item.content),
+        );
+      },
+    );
   }
 }
--- lib/news_view_model.dart
+++ lib/news_view_model.dart
@@ -4,7 +4,7 @@ import 'package:zinnia/api/response.dart';
 import 'package:zinnia/repository/news_repository.dart';
 
 final newsViewModelProvider = ChangeNotifierProvider(
-      (ref) => NewsViewModel(ref.watch(newsRepositoryProvider))..init(),
+  (ref) => NewsViewModel(ref.watch(newsRepositoryProvider))..init(),
 );
 
 class NewsViewModel extends ChangeNotifier {
@@ -12,13 +12,39 @@ class NewsViewModel extends ChangeNotifier {
 
   final NewsRepository _newsRepository;
 
+  List<NewsItemViewModel> _items = [];
+
+  List<NewsItemViewModel> get items => _items;
+
   @visibleForTesting
   Future<void> init() async {
     final News news;
     try {
-      news = await _newsRepository.findAll();
+      news = await _newsRepository.find(country: 'us');
     } catch (e) {
       return;
     }
+    _items = news.articles
+        .map(
+          (e) => NewsItemViewModel(
+            title: e.title,
+            content: e.content ?? '',
+            url: e.url,
+          ),
+        )
+        .toList(growable: false);
+    notifyListeners();
   }
+}
+
+class NewsItemViewModel {
+  NewsItemViewModel({
+    required this.title,
+    required this.content,
+    required this.url,
+  });
+
+  final String title;
+  final String content;
+  final String url;
 }
