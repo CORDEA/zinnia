dd67a718ae3ce3544b4d4da95aedcacaae5fd975
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-14 14:47:22 +0900

    add tab bar

--- lib/home.dart
+++ lib/home.dart
@@ -7,23 +7,43 @@ import 'home_view_model.dart';
 class HomePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const NewsPage(),
-      bottomNavigationBar: HookConsumer(
-        builder: (context, ref, child) => BottomNavigationBar(
-          currentIndex: ref.watch(
-            homeViewModelProvider.select((value) => value.navigationIndex),
-          ),
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: 'News'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.category),
-              label: 'Category',
+    final shouldShowTab = ref.watch(
+      homeViewModelProvider.select((value) => value.shouldShowTab),
+    );
+    final tabs = ref.watch(
+      homeViewModelProvider.select((value) => value.tabs),
+    );
+    return DefaultTabController(
+      length: shouldShowTab ? tabs.length : 1,
+      child: Scaffold(
+        appBar: shouldShowTab
+            ? AppBar(
+                title: const Text('Home'),
+                bottom: TabBar(
+                  isScrollable: true,
+                  tabs: tabs.map((e) => Tab(text: e)).toList(),
+                ),
+              )
+            : AppBar(title: const Text('Home')),
+        body: const NewsPage(),
+        bottomNavigationBar: HookConsumer(
+          builder: (context, ref, child) => BottomNavigationBar(
+            currentIndex: ref.watch(
+              homeViewModelProvider.select((value) => value.navigationIndex),
             ),
-          ],
-          onTap: (index) =>
-              ref.read(homeViewModelProvider).onNavigationItemTapped(index),
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list_alt),
+                label: 'News',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.category),
+                label: 'Category',
+              ),
+            ],
+            onTap: (index) =>
+                ref.read(homeViewModelProvider).onNavigationItemTapped(index),
+          ),
         ),
       ),
     );
--- lib/home_view_model.dart
+++ lib/home_view_model.dart
@@ -4,9 +4,23 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final homeViewModelProvider = ChangeNotifierProvider((_) => HomeViewModel());
 
 class HomeViewModel extends ChangeNotifier {
+  static const _categories = {
+    'Business': 'business',
+    'Entertainment': 'entertainment',
+    'General': 'general',
+    'Health': 'health',
+    'Science': 'science',
+    'Sports': 'sports',
+    'Technology': 'technology',
+  };
+
   int _navigationIndex = 0;
 
   int get navigationIndex => _navigationIndex;
+
+  bool get shouldShowTab => _navigationIndex == 1;
+
+  List<String> get tabs => _categories.keys.toList(growable: false);
 
   void onNavigationItemTapped(int index) {
     _navigationIndex = index;
